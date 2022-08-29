d74d43f1f078d209b854ce567a847afdf014399c
Edward <ejs910@gmail.com>
2022-08-29 19:03:13 -0400

    have a commit before i start messing with rspec configuration more

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+CSV.foreach(Rails.root.join('db/options_seeds', headers: true)) do |row|
+    
+    row_prompt = row['prompt']
+    question = Question.find_by(prompt: row_prompt)
+    #if the prompt is not already in the db, create a new Question
+    question = Question.create(prompt: row_prompt) if !question 
+
+    
+    Option.create(question_id: question.id,
+    image: row['name'] + question.id.to_s)
+    
+
+
+end
+
+def db_row_from_csv_row
